168d5358a18168d26ece5cb6de75a30d1dcbdf20
chimereya <chizzychimex84@gmail.com>
2023-10-26 14:32:14 +0100

    added comment and reply system

--- core/forms.py
+++ core/forms.py
@@ -1,13 +1,18 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['question',]
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
         
 
         
--- core/models.py
+++ core/models.py
@@ -24,6 +24,28 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("core:detail", kwargs={"pk": self.pk})
 
+    def get_comments(self):
+        return self.comments.filter(parent=None).filter(active=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, related_name="comments")
+    name = models.CharField(max_length=50, null=True)
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('date_created',)
+
+    def __str__(self):
+        return self.body
+
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
+
     
 
     
